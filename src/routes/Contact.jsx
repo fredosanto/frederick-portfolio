ede2f667e539f0e52b6a15df53a65574372cd4a7
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-12-17 23:46:51 +0100

    feat: contact and about page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
+import "../styles/main.scss";
+import "../styles/contact.scss";
+import { SocialBar } from "../components/layout/SocialBar";
+
+function ContactSection() {
+  return (
+    <div className="contact-wrapper">
+      <Link to="/" className="back-btn">
+        Go to homepage
+      </Link>
+      <h1>Contact</h1>
+      <p>
+        This page is still under construction, please visit my{" "}
+        <a href="https://www.linkedin.com/in/frederick-santos-agapay-734723115/">
+          LinkedIn
+        </a>{" "}
+        or <a href="https://github.com/fredosanto">GitHub</a> profile to get in
+        touch!
+      </p>
+    </div>
+  );
+}
+
 export function Contact() {
   return (
-    <>
-      <h1>Contact</h1>
-      <div>Contact me</div>
-    </>
+    <div className="container">
+      <SocialBar />
+      <ContactSection />
+    </div>
   );
 }
